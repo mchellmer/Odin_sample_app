a02776b3f80938a99856215261d834040c87b86c
mchellmer <mchellmer@gmail.com>
2016-04-01 16:03:47 +0200

    Added editing, authentication, admin access, deleting users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
 
   
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
   # Returns the hash digest of the given string.
   def User.digest(string)
